7b1d589b34a878154e96934f16bd1d20cae395e1
Antero Vainio <anterongugl@gmail.com>
2022-03-28 22:15:10 +0300

    Structure training phases into separate functions

--- src/training.py
+++ src/training.py
@@ -6,7 +6,7 @@ from datasets.mnist_fashion import load_mnist_fashion_dataset
 
 def get_device():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using {device} device")
+    print(f"\nUsing {device} device")
     return device
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -41,33 +41,40 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+
+def train_model(model, train_dataloader, loss_fn, optimizer, test_dataloader, epochs = 5):
+    print("\nTraining model")
+    for t in range(epochs):
+        print(f"\nEpoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
 
 def evaluate_model(model, dataloader, classes):
+    print("\nEvaluating model")
     model.eval()
     x, y = next(iter(dataloader))
     with torch.no_grad():
         pred = model(x[0])
         predicted, actual = classes[pred[0].argmax(0)], classes[y[0]]
-        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+        print(f'Predicted: "{predicted}", Actual: "{actual}"\n')
 
 def save_model(model, filepath):
     torch.save(model.state_dict(), filepath)
     print("Saved PyTorch Model State to {:s}".format(filepath))
 
-device = get_device()
-model = NeuralNetwork().to(device)
-train_dataloader, test_dataloader, classes = load_mnist_fashion_dataset()
+if __name__ == "__main__":
+    device = get_device()
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    model = NeuralNetwork().to(device)
+    train_dataloader, test_dataloader, classes = load_mnist_fashion_dataset()
 
-# Training
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
+    train_model(model,
+                train_dataloader,
+                nn.CrossEntropyLoss(),
+                torch.optim.SGD(model.parameters(), lr=1e-3),
+                test_dataloader)
 
-evaluate_model(model, test_dataloader, classes)
+    evaluate_model(model, test_dataloader, classes)
+
