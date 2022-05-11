dd7bd3c562866a50a0d06c328142fd578567d6fe
Diego Kiedanski <dkiedanski@gmail.com>
2022-05-10 20:04:21 -0400

    adding vgg and parametrizing models

--- examples/split_learning/split_training_server.py
+++ examples/split_learning/split_training_server.py
@@ -6,31 +6,34 @@ from sparse.roles.worker import Worker
 from sparse.roles.task_executor import TaskExecutor
 
 from serialization import decode_offload_request, encode_offload_response
-from models.neural_network import NeuralNetwork_server
+
+# from models.neural_network import NeuralNetwork_server
+from models.index import SECOND_SPLIT
 from utils import get_device
 
+
 class GradientCalculator(TaskExecutor):
-    def __init__(self):
+    def __init__(self, model_kind: str = "basic_nn"):
         super().__init__()
         self.device = get_device()
         self.loss_fn = nn.CrossEntropyLoss()
-        self.model = NeuralNetwork_server()
+        self.model = SECOND_SPLIT[model_kind]
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-3)
 
     def start(self):
-        """Initialize executor by transferring the model to the processor memory.
-        """
+        """Initialize executor by transferring the model to the processor memory."""
         super().start()
         self.logger.info(f"Task executor using {self.device} for processing")
         self.model.to(self.device)
         self.model.train()
 
-    def execute_task(self, input_data : bytes) -> bytes:
-        """Execute a single gradient computation for the offloaded layers.
-        """
+    def execute_task(self, input_data: bytes) -> bytes:
+        """Execute a single gradient computation for the offloaded layers."""
         # Input de-serialization
         split_layer, labels = decode_offload_request(input_data)
-        split_layer, labels = Variable(split_layer, requires_grad=True).to(self.device), labels.to(self.device)
+        split_layer, labels = Variable(split_layer, requires_grad=True).to(
+            self.device
+        ), labels.to(self.device)
         split_layer.retain_grad()
 
         # Finish forward pass
@@ -43,8 +46,9 @@ class GradientCalculator(TaskExecutor):
         self.optimizer.step()
 
         # Result serialization
-        return encode_offload_response(split_layer.grad.to('cpu').detach(), loss.item())
+        return encode_offload_response(split_layer.grad.to("cpu").detach(), loss.item())
+
 
 if __name__ == "__main__":
-    split_training_server = Worker(task_executor = GradientCalculator())
+    split_training_server = Worker(task_executor=GradientCalculator())
     split_training_server.start()
