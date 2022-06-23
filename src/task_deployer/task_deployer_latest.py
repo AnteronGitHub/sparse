3383172a2cdbfd3c7a5e3db24333bcd543f6ef79
Antero Vainio <anterongugl@gmail.com>
2022-06-23 14:06:40 +0300

    Retry failed connections to upstream after five seconds

--- src/task_deployer/task_deployer_latest.py
+++ src/task_deployer/task_deployer_latest.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from ..task_deployer import TaskDeployer
 
@@ -7,7 +8,13 @@ class TaskDeployerLatest(TaskDeployer):
     """
 
     async def stream_task_synchronous(self, input_data : bytes):
-        reader, writer = await asyncio.open_connection(self.upstream_host, self.upstream_port)
+        while True:
+            try:
+                reader, writer = await asyncio.open_connection(self.upstream_host, self.upstream_port)
+                break
+            except ConnectionRefusedError:
+                self.logger.error("Unable to connect to upstream host. Trying again in 5 seconds...")
+                time.sleep(5)
 
         writer.write(input_data)
         writer.write_eof()
