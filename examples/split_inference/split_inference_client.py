b123999e29bdc1283c68b5e17c78608e770361b0
Antero Vainio <anterongugl@gmail.com>
2022-08-16 11:26:50 +0300

    Add simple benchmark measurements to split inference client

--- examples/split_inference/split_inference_client.py
+++ examples/split_inference/split_inference_client.py
@@ -37,23 +37,31 @@ class SplitInferenceClient(Master):
                 # Load image to processor memory
                 img = ImageLoading(imagePath, self.img_size)
                 X = img.to(self.device)
+                load_time = time.time() - start_time
 
                 # Local forward propagation
                 split_vals = model(X)
+                local_processing_time = time.time() - start_time - load_time
 
                 # Offloaded layers
                 input_data = encode_offload_request(split_vals.to("cpu").detach())
+                encoding_time = time.time() - start_time - load_time - local_processing_time
+
                 result_data = await self.task_deployer.deploy_task(input_data)
+                server_processing_time = time.time() - start_time - load_time - local_processing_time - encoding_time
+
                 pred = decode_offload_response(result_data)
+                decoding_time = time.time() - start_time - load_time - local_processing_time - encoding_time - server_processing_time
 
                 #post process layers
                 conf_thres = 0.95 #object confidence threshold
                 nms_thres = 0.3 #iou thresshold for non-maximum suppression
                 detection = non_max_suppression(pred, conf_thres, nms_thres)
+                post_processing_time = time.time() - start_time - load_time - local_processing_time - encoding_time - server_processing_time - decoding_time
 
                 img = X
                 save_detection(img, imagePath, detection)
-                self.logger.info(f"Run object detection for image '{imagePath}' in {time.time() - start_time} seconds.")
+                self.logger.info(f"Run object detection for image '{imagePath}'. Wall clock time: {time.time() - start_time:.4f}s, loading time: {load_time:.4f}s, local inference time: {local_processing_time:.4f}s, encoding time: {encoding_time:.4f}s, offloaded processing time: {server_processing_time:.4f}s, decoding time: {decoding_time:.4f}s, post processing time: {post_processing_time:.4f}s.")
 
         self.logger.info("Done!")
 
