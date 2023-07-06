aa1379d1d4718c52566b912544be26cb2eec4e51
Antero Vainio <anterongugl@gmail.com>
2023-07-06 11:30:42 +0300

    Generate partitioned VGG models procedurally

--- sparse_framework/dl/models/vgg.py
+++ sparse_framework/dl/models/vgg.py
@@ -25,10 +25,19 @@ class VGGClassifier(nn.Module):
 
         return x
 
+VGG_LAYERS = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
 class VGG(ModuleQueue):
-    def __init__(self, features, num_classes=1000, init_weights=False, local = False):
-        partitions = [features]
-        if not local:
+    def __init__(self, variant="A", start=0, end=-1, num_classes=1000, init_weights=False, wrap_layers=False):
+        if wrap_layers:
+            partitions = nn.Sequential(*make_layers(variant, start, end))
+        else:
+            partitions = make_layers(variant, start, end)
+        if end < 0 or end > len(VGG_LAYERS[variant]):
             partitions.append(VGGClassifier(num_classes))
         super(VGG, self).__init__(partitions)
 
@@ -48,53 +57,32 @@ class VGG(ModuleQueue):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
 
-def make_layers(cfg, in_channels=3, batch_norm=True):
+def make_layers(variant, start, end, batch_norm=True):
     layers = []
-
-    for v in cfg:
+    in_channels = 3
+    for i, v in enumerate(VGG_LAYERS[variant]):
         if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            if i >= start and (i < end or end < 0):
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
+            if i >= start and (i < end or end < 0):
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
 
-    return nn.Sequential(*layers)
-
-cfg = {
-    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-}
-cfg_local = {
-    #'A': [64, 'M', 128, 'M'],
-    #'E': [64, 64, 'M', 128, 128],
-    'A': [64, 'M', 128, 'M'],
-    'D': [64],
-    'E': [64, 64, 'M', 128, 128],
-}
-cfg_server = {
-    #'A': [256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    #'E': ['M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-    'A': [256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'D': [64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'E': ['M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-}
+    return layers
 
 class VGG_unsplit(VGG):
     def __init__(self):
-        super().__init__(make_layers(cfg["A"]))
+        super().__init__()
 
 class VGG_server(VGG):
     def __init__(self):
-        super().__init__(make_layers(cfg_server["A"],
-                                     in_channels=128),
-                         local = False)
+        super().__init__(start = 4)
 
 class VGG_client(VGG):
     def __init__(self):
-        super().__init__(make_layers(cfg_local["A"]),
-                         local = True)
+        super().__init__(end = 4)
