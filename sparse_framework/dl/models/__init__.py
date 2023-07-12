83d67326ced794a88d9b9f817348532864176767
Antero Vainio <anterongugl@gmail.com>
2023-07-12 14:03:14 +0300

    Move module queue implementation into dl.models submodule

--- sparse_framework/dl/models/__init__.py
+++ sparse_framework/dl/models/__init__.py
@@ -1,7 +1,9 @@
+from .module_queue import ModuleQueue
 from .vgg import VGG_client, VGG_server, VGG_unsplit
 from .small import Small_client, Small_server, Small_unsplit
 
-__all__ = ["VGG_client",
+__all__ = ["ModuleQueue",
+           "VGG_client",
            "VGG_server",
            "VGG_unsplit",
            "Small_client",
