b20a47e153fed39a745dc21f4e4ecdde774597b0
Antero Vainio <anterongugl@gmail.com>
2023-10-18 10:36:52 +0300

    Initialize dataloader in datasource constructor

--- examples/splitnn/datasource.py
+++ examples/splitnn/datasource.py
@@ -15,7 +15,7 @@ TARGET_FPS = 5.0
 
 class ModelClientProtocol(asyncio.Protocol):
     def __init__(self, data_source_id, dataset, model_meta_data, on_con_lost, no_samples = 64, target_rate = 1/TARGET_FPS):
-        self.dataset = dataset
+        self.dataloader = DataLoader(dataset, 1)
         self.model_meta_data = model_meta_data
         self.on_con_lost = on_con_lost
         self.no_samples = no_samples
@@ -27,6 +27,7 @@ class ModelClientProtocol(asyncio.Protocol):
 
         self.latencies = []
         self.request_latencies = []
+        self.ratios = []
 
     def initialize_stream(self):
         self.processing_started = time()
@@ -41,19 +42,20 @@ class ModelClientProtocol(asyncio.Protocol):
     def offload_task(self):
         self.processing_started = time()
         self.no_samples -= 1
-        for batch, (X, y) in enumerate(DataLoader(self.dataset, 1)):
-            self.transport.write(pickle.dumps({ 'op': 'offload_task',
-                                                'activation': X,
-                                                'labels': y,
-                                                'model_meta_data': self.model_meta_data }))
-            self.last_sent_at = time()
-            break
+        features, labels = next(iter(self.dataloader))
+        self.transport.write(pickle.dumps({ 'op': 'offload_task',
+                                            'activation': features,
+                                            'labels': labels,
+                                            'model_meta_data': self.model_meta_data }))
+        self.last_sent_at = time()
 
     def offload_task_completed(self, result_data):
         latency = time() - self.processing_started
         request_latency = time() - self.last_sent_at
+
         self.latencies.append(latency)
         self.request_latencies.append(request_latency)
+        self.ratios.append(request_latency/latency)
 
         if (self.no_samples > 0):
             loop = asyncio.get_running_loop()
@@ -68,7 +70,8 @@ class ModelClientProtocol(asyncio.Protocol):
         else:
             avg_latency = sum(self.latencies)/len(self.latencies)
             avg_request_latency = sum(self.request_latencies)/len(self.request_latencies)
-            self.logger.info(f"{no_requests} offloaded tasks, avg framerate / avg req lat.: {1.0/avg_latency:.2f} FPS / {1000*avg_request_latency:.2f} ms.")
+            avg_ratio = sum(self.ratios)/len(self.ratios)
+            self.logger.info(f"Stream statistics: {no_requests} tasks / {1.0/avg_latency:.2f} avg FPS / {1000*avg_request_latency:.2f} ms avg request latency / {100.0*avg_ratio:.2f} % avg offload latency ratio.")
 
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
