a683b6ed7f204179791fe523d9bb789ec897a8dc
Antero Vainio <anterongugl@gmail.com>
2024-09-17 13:48:29 +0300

    Move api client to utility module

--- sparse_framework/__init__.py
+++ sparse_framework/__init__.py
@@ -1,12 +1,10 @@
 # This relies on each of the submodules having an __all__ variable.
 from .node import *
-from .sparse_api_client import SparseAPIClient
 from .stats import *
 from .stream_api import *
 from .utils import *
 
-__all__ = ["SparseAPIClient",
-           stream_api.__all__,
+__all__ = [stream_api.__all__,
            node.__all__,
            stats.__all__,
            utils.__all__]
--- sparse_framework/utils/__init__.py
+++ sparse_framework/utils/__init__.py
@@ -1,9 +1,11 @@
 """This module includes utility functionality that is not required to run a cluster but can be helpful for development
 and testing.
 """
+from .api_client import SparseAPIClient
 from .sink import SparseSink
 from .source import SourceProtocol, SparseSource
 
-__all__ = ["SourceProtocol",
+__all__ = ["SparseAPIClient",
+           "SourceProtocol",
            "SparseSink",
            "SparseSource"]
