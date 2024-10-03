6900c6b029a1f4e6307407466e5c531b3fbb7f45
Antero Vainio <anterongugl@gmail.com>
2024-10-03 12:52:04 +0300

    Add option to parse deployment from yaml files

--- sparse_framework/deployment.py
+++ sparse_framework/deployment.py
@@ -1,3 +1,5 @@
+import yaml
+
 class Deployment:
     name : str
     dag : dict
@@ -8,3 +10,12 @@ class Deployment:
 
     def __str__(self):
         return self.name
+
+    @classmethod
+    def from_yaml(cls, file_path : str):
+        with open(file_path) as f:
+            data = yaml.safe_load(f)
+
+        for k in data["dag"].keys(): data["dag"][k] = set(data["dag"][k])
+
+        return cls(data["name"], data["dag"])
